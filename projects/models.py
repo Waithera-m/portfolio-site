a36ebcbcc3af9ab4637e73f3a58a27b864a9e708
Mary Waithera <njihiamary11@gmail.com>
2020-05-21 14:13:18 +0300

    create repo model and test method to test the initiaization of its objects

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Repo(models.Model):
+
+    """
+    class facilitates the creation of project objects
+    """
+    project_name = models.CharField(max_length=70)
+    description = models.TextField(max_length=300)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    url = models.CharField(max_length=100)
--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Repo
+import datetime
 
-# Create your tests here.
+class RepoModelTests(TestCase):
+
+    """
+    class facilitates the creation of repo objects
+    """
+    def setUp(self):
+
+        """
+        method runs before all tests
+        """
+        self.repo = Repo('Partage','Partage is a simple flask application that allows users to view and comment on posts. Users can also use provided links to add new posts, update and delete existing posts','2020-05-21','https://github.com/Waithera-m/partage')
+
+    def test_init(self):
+
+        """
+        method checks if objects are initialized properly
+        """
+        self.assertTrue(isinstance(self.repo,Repo))
